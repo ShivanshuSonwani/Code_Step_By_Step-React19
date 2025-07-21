57a697082687d965dcbe45dd107e2753f666e0a1
ShivanshuSonwani <shivanshu.ed@gmail.com>
2025-07-21 14:59:34 +0530

    first basic project

---
+++ 01_react_project/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Hello React</h1>
+      <h2>Code Step by Step</h2>
+    </div>
+  )  
+}
+
+export default App
